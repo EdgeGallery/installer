d766328d27a69f35ae6da21c995e35dfb9235f70
姜伟 <jwjiang_seu@163.com>
2020-11-17 16:39:23 +0800

    update offline/eg.sh.

--- offline/eg.sh
+++ offline/eg.sh
@@ -70,7 +70,7 @@ function _docker_deploy() {
 Description=Docker Daemon
 
 [Service]
-ExecStart=/usr/bin/dockerd
+ExecStart=/usr/bin/dockerd --bip "192.168.1.1/24"
 
 [Install]
 WantedBy=multi-user.target
