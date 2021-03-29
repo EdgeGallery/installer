082b32620add1494721ebd00c249b6718157bfda
赵宝辉 <7624663+zhaobaohui@user.noreply.gitee.com>
2021-03-29 11:19:56 +0800

    modified mepm-fe name

--- offline/eg.sh
+++ offline/eg.sh
@@ -1052,17 +1052,17 @@ function install_mecm-mepm ()
     --set images.lcmcontroller.repository=$mepm_images_lcmcontroller_repository \
     --set images.k8splugin.repository=$mepm_images_k8splugin_repository \
     --set images.apprulemgr.repository=$mepm_images_apprulemgr_repository \
-    --set images.fe.repository=$mepm_images_fe_repository \
+    --set images.mepmFe.repository=$mepm_images_fe_repository \
     --set images.postgres.repository=$mepm_images_postgres_repository \
     --set images.lcmcontroller.tag=$mepm_images_lcmcontroller_tag \
     --set images.k8splugin.tag=$mepm_images_k8splugin_tag \
     --set images.apprulemgr.tag=$mepm_images_apprulemgr_tag \
-    --set images.fe.tag=$mepm_images_fe_tag \
+    --set images.mepmFe.tag=$mepm_images_fe_tag \
     --set images.postgres.tag=$mepm_images_postgres_tag \
     --set images.lcmcontroller.pullPolicy=$mepm_images_lcmcontroller_pullPolicy \
     --set images.k8splugin.pullPolicy=$mepm_images_k8splugin_pullPolicy \
     --set images.apprulemgr.pullPolicy=$mepm_images_apprulemgr_pullPolicy \
-    --set images.fe.pullPolicy=$mepm_images_fe_pullPolicy \
+    --set images.mepmFe.pullPolicy=$mepm_images_fe_pullPolicy \
     --set images.postgres.pullPolicy=$mepm_images_postgres_pullPolicy \
     --set global.persistence.enabled=$ENABLE_PERSISTENCE
     if [ $? -eq 0 ]; then
