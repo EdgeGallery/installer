0c1472ec39c0b776f609819e4239b6b252994b79
许丹 <xudan16@huawei.com>
2021-04-01 16:11:03 +0800

    !158 add rule mgmt database in mecm-osplugin * add rule mgmt database in mecm-osplugin

--- ansible_package/roles/init/files/conf/keys/postgres_init.sql
+++ ansible_package/roles/init/files/conf/keys/postgres_init.sql
@@ -58,3 +58,12 @@ CREATE DATABASE osplugindb
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 
+CREATE USER apprulemgr WITH PASSWORD 'te9Fmv%qaq' CREATEDB;
+CREATE DATABASE apprulemgrdb
+    WITH
+    OWNER = apprulemgr
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
