e55f283834cfefcd3868c28e84896dd74e7e290e
zhanghailong22 <zhanghailong22@huawei.com>
2021-04-01 11:21:27 +0800

    新增rule数据库

--- offline/conf/keys/postgres_init.sql
+++ offline/conf/keys/postgres_init.sql
@@ -57,4 +57,14 @@ CREATE DATABASE osplugindb
     LC_CTYPE = 'en_US.utf8'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
+	
+CREATE USER apprulemgr WITH PASSWORD 'te9Fmv%qaq' CREATEDB;
+CREATE DATABASE apprulemgrdb
+    WITH 
+    OWNER = apprulemgr
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
 
