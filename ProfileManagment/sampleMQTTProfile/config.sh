800fef696a98b2d70015e06050e41ec94bfa7f3f
VictorGao <victor.gao@huawei.com>
2021-10-10 02:41:32 +0000

    !377 Add MQTTsample profile * Add Sample MQTT Profile

---
+++ ProfileManagment/sampleMQTTProfile/config.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+
+FLEDGE_HOST='http://0.0.0.0:8081'
+KUIPER_HOST='http://0.0.0.0:9081'
+MQTT_TDENG_ADAPTER_HOST='http://0.0.0.0:9085'
+MQTT_BROKER_HOST='mqtt-broker'
+BROKER_PORT='1883'
+
+
+
+
+
