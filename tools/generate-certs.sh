cbff5f3173e1c80ded754153aa2aa28902463aec
许丹 <xudan16@huawei.com>
2021-10-19 09:05:32 +0000

    !390 update deplot-tool dockerfile to make it smaller

--- tools/generate-certs.sh
+++ tools/generate-certs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#   Copyright 2020 Huawei Technologies Co., Ltd.
+#   Copyright 2021 Huawei Technologies Co., Ltd.
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -15,36 +15,30 @@
 #   limitations under the License.
 #
 
-KEYS_DIR=$PWD
-rm -rf /tmp/ssl-eg-keys-certs
-mkdir -p /tmp/ssl-eg-keys-certs
-cd /tmp/ssl-eg-keys-certs || exit
+#!/bin/bash
 
-## Generate a RSA keypair through openssl
+set -e
+
+echo "preparing ssl certs"
+
+mkdir -p /tmp/ssl-eg-keys-certs
+cd /tmp/ssl-eg-keys-certs
 openssl genrsa -out rsa_private_key.pem 2048
 openssl rsa -in rsa_private_key.pem -pubout -out rsa_public_key.pem
-openssl pkcs8 -topk8 -inform PEM -in rsa_private_key.pem -outform PEM -passout pass:te9Fmv%qaq -out encrypted_rsa_private_key.pem
-
-## Generate ca certificate
+openssl pkcs8 -topk8 -inform PEM -in rsa_private_key.pem -outform PEM -passout pass:$CERT_PASSWORD -out encrypted_rsa_private_key.pem
 openssl genrsa -out ca.key 2048
 openssl req -new -key ca.key -subj /C=CN/ST=Beijing/L=Biejing/O=edgegallery/CN=edgegallery.org -out ca.csr
-openssl x509 -req -days 365 -in ca.csr -extensions v3_ca -signkey ca.key -out ca.crt
-
-## Generate tls certificate
+openssl x509 -req -days $CERT_VALIDITY_IN_DAYS -in ca.csr -extensions v3_ca -signkey ca.key -out ca.crt
 openssl genrsa -out tls.key 2048
-openssl rsa -in tls.key -aes256 -passout pass:te9Fmv%qaq -out encryptedtls.key
+openssl rsa -in tls.key -aes256 -passout pass:$CERT_PASSWORD -out encryptedtls.key
 openssl req -new -key tls.key -subj /C=CN/ST=Beijing/L=Biejing/O=edgegallery/CN=edgegallery.org -out tls.csr
-openssl x509 -req -in tls.csr -extensions v3_usr -CA ca.crt -CAkey ca.key -CAcreateserial -out tls.crt
-
-## Generate a keystore.p12 through keytool
+openssl x509 -req -days $CERT_VALIDITY_IN_DAYS -in tls.csr -extensions v3_usr -CA ca.crt -CAkey ca.key -CAcreateserial -out tls.crt
 keytool -genkey -alias edgegallery \
   -dname "CN=edgegallery,OU=edgegallery,O=edgegallery,L=edgegallery,ST=edgegallery,C=CN" \
-  -storetype PKCS12 -keyalg RSA -keysize 2048 -storepass te9Fmv%qaq -keystore keystore.p12 -validity 365
-
-yes yes | keytool -importcert -file ca.crt -alias clientkey -keystore keystore.jks -storepass te9Fmv%qaq
-
+  -storetype PKCS12 -keyalg RSA -keysize 2048 -storepass $CERT_PASSWORD -keystore keystore.p12 -validity $CERT_VALIDITY_IN_DAYS
+yes yes | keytool -importcert -file ca.crt -alias clientkey -keystore keystore.jks -storepass $CERT_PASSWORD
+openssl pkcs12 -in keystore.p12 -nokeys -clcerts -out server.crt -passin pass:$CERT_PASSWORD
+openssl pkcs12 -nocerts -nodes -in keystore.p12 -out server.key -passin pass:$CERT_PASSWORD
 rm ca.csr ca.key ca.srl tls.csr
-
-cp -r /tmp/ssl-eg-keys-certs/* $KEYS_DIR
+cp -r /tmp/ssl-eg-keys-certs/* /certs
 rm -rf /tmp/ssl-eg-keys-certs/
-exit 0
